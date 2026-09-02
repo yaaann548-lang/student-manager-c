556e27cc2dcfdc5794e5292d3fa3034703b6e2ff
yaaann548-lang <yaaann54.8@gmail.com>
2026-09-02 15:54:26 +0800

    feat: rename program title

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(void)
 {
     Student student;
 
-    printf("=== Student Management System ===\n");
+    printf("=== Student Information System ===\n");
 
     printf("Enter student ID: ");
     scanf("%d", &student.id);
